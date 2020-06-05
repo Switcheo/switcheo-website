589a5f64fc60cdae2e6c6395f1125b372830466e
Ivan Poon <ravenxce@gmail.com>
2020-06-05 16:18:05 +0800

    Fix typo

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -18,7 +18,7 @@ module.exports = {
       path : '/var/www/switcheo-website',
       'pre-deploy-local': '',
       'pre-setup' : 'sudo npm install pm2@latest -g; sudo npm install yarn -g; ssh-keyscan -t rsa github.com >> ~/.ssh/known_hosts;',
-      'post-deploy' : 'yarn install && yarn build && pm2 startOrRestart ecosystem.json --env production',
+      'post-deploy' : 'yarn install && yarn build && pm2 startOrRestart ecosystem.config.js --env production',
     }
   }
 };
