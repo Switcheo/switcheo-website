0c50368cfbf60bcaef5844708e3deb45d98a9e1d
Steven Khong <khongwh@gmail.com>
2021-04-16 10:20:56 +0800

    Fix origin redirect issue

--- src/server/middleware/html.ts
+++ src/server/middleware/html.ts
@@ -1,7 +1,6 @@
 import { NextFunction, Request, Response } from 'express';
 import fs from 'fs';
 import path from 'path';
-import { CANONICAL_ORIGIN } from './constants';
 
 const htmlMiddleware = () => (
   req: Request,
@@ -9,12 +8,6 @@ const htmlMiddleware = () => (
   next: NextFunction
 ) => {
   const publicPath = path.join(__dirname, '/public');
-
-  const host = req.header("host");
-  if (process.env.NODE_ENV === "production" && host !== CANONICAL_ORIGIN) {
-    res.redirect(`${CANONICAL_ORIGIN}${req.originalUrl}`);
-    return;
-  }
 
   fs.readFile(`${publicPath}/app.html`, 'utf8', (err, html) => {
     if (!err) {
