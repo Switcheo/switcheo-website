6744a3f211749cb9a123d26c34cab1de46026cf9
Steven Khong <khongwh@gmail.com>
2021-09-16 13:34:46 +0800

    Update pm2 environments

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,44 +1,51 @@
 module.exports = {
-  apps : [{
-    name: "switcheo-website",
-    script: 'build/index.js',
-    instances : "max",
-    exec_mode : "cluster",
+  apps: [{
+    name: "switcheo-website-" + process.env.PM2_NAME,
+    ...process.env.PM2_NAME === "production" && {
+      script: 'build/index.js',
+    },
+    ...process.env.PM2_NAME === "staging" && {
+      script: 'yarn',
+      args: 'start',
+    },
+    args: 'start',
+    instances: "max",
+    exec_mode: "cluster",
     node_args: [
       "--max_old_space_size=4096"
     ],
-    env_production : {
+    env_production: {
       "PORT": 5000,
       "ENV": "production",
       "NODE_ENV": "production"
     },
-    env_staging : {
+    env_staging: {
       "PORT": 4000,
       "ENV": "production",
       "NODE_ENV": "production"
     },
   }],
 
-  deploy : {
-    production : {
-      user : 'ubuntu',
-      host : 'switcheo-website',
-      ref  : 'origin/master',
-      repo : 'git@github.com:Switcheo/switcheo-website.git',
-      path : '/var/www/switcheo-website',
+  deploy: {
+    production: {
+      user: 'ubuntu',
+      host: 'switcheo-website',
+      ref: 'origin/master',
+      repo: 'git@github.com:Switcheo/switcheo-website.git',
+      path: '/var/www/switcheo-website',
       'pre-deploy-local': '',
-      'pre-setup' : 'sudo npm install pm2@latest -g; sudo npm install yarn -g; ssh-keyscan -t rsa github.com >> ~/.ssh/known_hosts;',
-      'post-deploy' : 'yarn install && yarn build && cp ~/.contentful.env /var/www/switcheo-website/current/.env && pm2 startOrRestart ecosystem.config.js --env production',
+      'pre-setup': 'sudo npm install pm2@latest -g; sudo npm install yarn -g; ssh-keyscan -t rsa github.com >> ~/.ssh/known_hosts;',
+      'post-deploy': 'yarn install && yarn build && cp ~/.contentful.env /var/www/switcheo-website/current/.env && PM2_NAME=production pm2 startOrRestart ecosystem.config.js --env production',
     },
-    staging : {
-      user : 'ubuntu',
-      host : 'switcheo-website',
-      ref  : 'origin/staging',
-      repo : 'git@github.com:Switcheo/switcheo-website.git',
-      path : '/var/www/switcheo-website-staging',
+    staging: {
+      user: 'ubuntu',
+      host: 'switcheo-website',
+      ref: 'origin/staging',
+      repo: 'git@github.com:Switcheo/switcheo-website.git',
+      path: '/var/www/switcheo-website-staging',
       'pre-deploy-local': '',
-      'pre-setup' : 'sudo npm install pm2@latest -g; sudo npm install yarn -g; ssh-keyscan -t rsa github.com >> ~/.ssh/known_hosts;',
-      'post-deploy' : 'yarn install && yarn build && cp ~/.contentful.env /var/www/switcheo-website-staging/current/.env && pm2 startOrRestart ecosystem.config.js --env staging',
+      'pre-setup': 'sudo npm install pm2@latest -g; sudo npm install yarn -g; ssh-keyscan -t rsa github.com >> ~/.ssh/known_hosts;',
+      'post-deploy': 'yarn install && yarn build && cp ~/.contentful.env /var/www/switcheo-website-staging/current/.env && PM2_NAME=staging pm2 startOrRestart ecosystem.config.js --env staging',
     }
   }
 };
