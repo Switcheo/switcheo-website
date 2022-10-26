398d47e50955ba037560bb89ef03a1be8d26569b
Ivan Poon <ivan.poon@switcheo.network>
2022-10-26 13:08:51 +0800

    Update main subtitle

--- src/components/Home/Hero/Hero.tsx
+++ src/components/Home/Hero/Hero.tsx
@@ -15,7 +15,7 @@ const Hero: React.FC = () => {
         </Box>
         <Box className={classes.description}>
           <Typography variant="subtitle1" align="center" color="textSecondary">
-          Switcheo Labs is a creative, experimental think-tank and software development house that focuses on cultivating the foundations of a new decentralized world.
+            Switcheo is an experimental think tank and cutting-edge software development lab that focuses on cultivating the foundations of a new, decentralized world.
           </Typography>
         </Box>
         <HeroAnimation />
