7274d8aba7fdfebd0220a40e13ebaef7e55e46df
Ivan Poon <ivan.poon@switcheo.network>
2022-10-26 15:20:10 +0800

    Update blog section and seo

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,18 +2,19 @@ import { createClient } from "contentful";
 import type { GetServerSideProps, InferGetServerSidePropsType, NextPage } from "next";
 import { NextSeo } from "next-seo";
 import { Blog, DeveloperUpdates, Hero, InnovationAreas, JoinUs, OurVision, Partners, Stats, WhoWeAre } from "src/components/Home";
+import { BlogEntry } from "src/utils/types";
 
-const Home: NextPage = ({ blogEntries,tweets,updatesEntries }: InferGetServerSidePropsType<typeof getServerSideProps>) => {
+const Home: NextPage = ({ blogEntries, tweets, updatesEntries }: InferGetServerSidePropsType<typeof getServerSideProps>) => {
   return (
     <>
       <NextSeo
         title="Switcheo Labs: Blockchain Innovation & Infrastructure"
-        description="Switcheo Labs is a creative and experimental think tank that nurtures ecosystems which form the foundations of a new decentralized world."
+        description="Switcheo Labs is an experimental think tank and cutting-edge software development lab that focuses on cultivating the foundations of a new, decentralized world."
         canonical="https://www.switcheo.com"
         openGraph={{
           url: "https://www.switcheo.com",
           title: "Switcheo Labs: Blockchain Innovation & Infrastructure",
-          description: "Switcheo Labs is a creative and experimental think tank that nurtures ecosystems which form the foundations of a new decentralized world.",
+          description: "Switcheo Labs is an experimental think tank and cutting-edge software development lab that focuses on cultivating the foundations of a new, decentralized world.",
           images: [{
             url: "https://switcheo-assets.s3.ap-southeast-1.amazonaws.com/switcheo-finance-without-limits.png",
           }],
@@ -45,14 +46,14 @@ export const getServerSideProps: GetServerSideProps = async () => {
 
   const client = createClient({ space, accessToken });
 
-  const blogEntryResult = await client.getEntries({
+  const blogEntryResult = await client.getEntries<BlogEntry>({
     content_type: "blogEntry",
     order: "-fields.date,sys.createdAt",
     limit: 10,
   });
 
   //TODO: Make a section for Update Entries on Contentful
-  const updates = await client.getEntries({
+  const updates = await client.getEntries<BlogEntry>({
     content_type: "blogEntry",
     order: "-fields.date,sys.createdAt",
     limit: 100,
@@ -63,8 +64,8 @@ export const getServerSideProps: GetServerSideProps = async () => {
     limit: 10,
   });
 
-  const blogEntries = (blogEntryResult.items.map((item) => item.fields));
-  const updatesEntries = (updates.items.map((item) => item.fields).filter((o:any) => o?.title?.includes('Update')));
+  const blogEntries = (blogEntryResult.items.map((item) => item.fields)).filter((o: BlogEntry) => !o?.title?.includes("Update"));
+  const updatesEntries = (updates.items.map((item) => item.fields).filter((o: BlogEntry) => o?.title?.includes("Update")));
   const tweets = (tweetResult.items.map((item) => item.fields));
 
   return {
